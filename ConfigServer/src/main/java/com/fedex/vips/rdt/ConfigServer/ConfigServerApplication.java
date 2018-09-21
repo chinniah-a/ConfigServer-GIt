66aa3c004b36d91cf9463c02a7e6c0812538afe2
chinniah-a <chinniah.a@accoliteindia.com>
2018-09-21 12:10:59 +0530

    Added Project to git

---
+++ ConfigServer/src/main/java/com/fedex/vips/rdt/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.fedex.vips.rdt.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
